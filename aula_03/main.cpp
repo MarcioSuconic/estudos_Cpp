426b7a366cd60b13e2e2330e305e4bc859c122f6
MarcioSuconic <advmsuconic@gmail.com>
2025-03-20 08:14:50 -0300

    first commit

---
+++ aula_03/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Ola mundo!" << endl ;
+    // \n faz o mesmo que o endl
+    cout << "Ola mundo!\nCurso de C++";
+    system("pause");
+    return 0;
+}
