e81c9edfd2c4394bb6799b177d6d870acc686f01
MarcioSuconic <advmsuconic@gmail.com>
2025-03-20 22:53:43 -0300

    05

--- aula_04_cout_cin/main.cpp
+++ aula_04_cout_cin/main.cpp
@@ -6,23 +6,25 @@ int main() {
     // tipo nome;
     // tipo nome = valor;
     int vidas_1 = 0; // recebe numeros inteiros e podem ser negativos
-    int vidas_2; // recebe numeros inteiros e podem ser negativos
+    int vidas_2 = 34; // recebe numeros inteiros e podem ser negativos
     char letra_1='B'; // recebe 1 caracter, uma letra
     //char letra_2[20]='Marcio'; // recebe até 20 caracteres, [] é um vetor, cada letra fica num vetor
     double decimal_1=2.666666666666666; // recebe numero decimal
     float decimal_2=2.6666666666666; // igual ao double mas tem uma precisao menor que o double
     bool vivo=true; // verdadeiro ou falso --> true false --> 0 e 1
-    string nome = "Suconic"; // recebe um texto
+    string nome = "Marcio Suconic"; // recebe um texto
 
     // se imprimir o valor de vidas, vai imprimir o ZERO
-    cout << vidas_1 << endl;
-    cout << vidas_2 << '\n';
-    cout << letra_1 << endl;
-    //cout << letra_2 << '\n';
-    cout << decimal_1 << endl;
-    cout << decimal_2 << '\n';
-    cout << vivo << endl;
-    cout << nome << '\n';
+    cout << "Digite a qtde de vidas 1: ";
+    cin >> vidas_1;
+    cout << "Digite a qtde de vidas 2: ";
+    cin >> vidas_2;
+    // cout << letra_1 << endl;
+    // cout << decimal_1 << endl;
+    // cout << decimal_2 << '\n';
+    // cout << vivo << endl;
+    // cout << nome << '\n';
 
+    cout << vidas_1 << endl << vidas_2 << '\n' << letra_1 << endl << decimal_1 << endl << decimal_2 << '\n' << "teste" << vivo << '\n' << "O nome é: " << nome << '\n';
     return 0;
 }
---
+++ aula_05_define/main.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+#define pi 3.141516
+#define canal cout << "\nCanal da HARMONY SOUND VISION\n\n";
+
+int main() {
+
+    int vidas, tiros, life; // declaraões múltiplas
+
+    int vidas2; // declarações individuais
+    int tiros2;
+    int life2;
+
+    cin >> vidas;
+    cin >> tiros;
+    cin >> life; 
+
+    cout << vidas;
+    cout << tiros;
+    cout << life << '\n'; 
+
+    cin >> vidas2;
+    cin >> tiros2;
+    cin >> life2; 
+
+    cout << vidas2;
+    cout << tiros2;
+    cout << life2 << '\n'; 
+
+    cout << pi;
+
+    canal
+
+    return 0;
+}
