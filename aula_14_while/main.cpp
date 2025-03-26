71d3d2d318248fae79810fdf72fd651c41f9d153
MarcioSuconic <advmsuconic@gmail.com>
2025-03-26 08:33:24 -0300

    do while

---
+++ aula_14_while/main.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int cont;
+
+    cont = 0;
+
+    while (cont < 20) {
+        cout << "Marcio Suconic - " << cont << "\n";
+        cont++;
+    }
+
+    cout << "Rotina finalizada.\n";
+
+    cont = 0;
+
+    do {
+        cout << "Marcio Suconic - " << cont << "\n";
+        cont++;
+    } while
+    ( cont < 20 );
+
+    cout << "Rotina finalizada.\n";
+
+    return 0;
+}
---
+++ aula_16_do_while/main.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+
+    int cont;
+
+    cont = 0;
+
+    while (cont < 20) {
+        cout << "Marcio Suconic - " << cont << "\n";
+        cont++;
+    }
+
+    return 0;
+}
+
+
+
+// do while testa no final
+
