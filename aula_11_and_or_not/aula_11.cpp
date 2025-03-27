0ae413ec7c910bf3a0381cfe3a67900c27de14ee
marcio suconic <advmsuconic@gmail.com>
2025-03-27 17:26:24 -0300

    aula 11

---
+++ aula_11_and_or_not/aula_11.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+\\ AND \\ && \\ E
+\\ OR \\ || \\ OU
+
+
+int main() {
+    int num;
+
+    num = 12;
+
+    if ( (num >= 3 && num <= 6) || (num > 9 && num < 15) || (num > 15 && num < 20)) {
+        cout << "\n\nValor aceito\n"
+    } else {
+        cout << "\n\nValor NÃO aceito\n"
+    }
+
+}
+
+
+
---
+++ aula_11_and_or_not/teste.py
@@ -0,0 +1,11 @@
+import matplotlib.pyplot as plt
+
+fig = plt.figure()
+ax = fig.add_subplot()
+
+ax.plot([1,2,3,4,5],[1,4,9,16,25])
+#ax.plot(self.lista_refs, self.listas_valores_brutos)
+plt.show()
+
+
+
